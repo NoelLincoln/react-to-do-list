628e58b255996936c071a3c8b79bfc26881e9584
Noel Bryant <noelsobryant@gmail.com>
2023-10-03 18:18:06 +0300

    create parent component ToDoApp

---
+++ src/components/ToDoApp.jsx
@@ -0,0 +1,13 @@
+import '@/App.css';
+import Header from './Header';
+import TodosLogic from './ToDosLogic';
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+};
+export default TodoApp;
