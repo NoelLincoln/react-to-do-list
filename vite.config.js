bc911eff95b98aa275debaa8974563e9c6e9b502
Noel Bryant <noelsobryant@gmail.com>
2023-10-03 18:16:06 +0300

    add configuration to resolve directory name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+
+const __dirname = path.dirname(__filename);
+console.log('directory-name', __dirname);
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
-})
+});
