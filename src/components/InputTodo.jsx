a7e228721f60aa61a5a9584d664052cc564733cc
Noel Bryant <noelsobryant@gmail.com>
2023-10-03 23:09:09 +0300

    create form to add items

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,10 @@
+const InputTodo = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter to do item"/>
+      </form>
+    </div>
+  );
+};
+export default InputTodo;
